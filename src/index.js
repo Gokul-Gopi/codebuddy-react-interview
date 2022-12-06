44780f543982d1d638d822b2a3307c6a4a05bcdf
Gokul Gopi <gokulg1412@gmail.com>
2022-12-06 16:43:13 +0530

    feat: fetch-rows api integrated, ui to input number of rows

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import App from './App';
 import worker from './mocks/browser';
 
 worker.start();
 
+const theme = extendTheme({
+  colors: {
+    primary: {
+      bg: '#051f27',
+    },
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,57 @@
-const Home = () => (
-  <main>
-    <span>Ticket booking app</span>
-  </main>
-);
+import { Box, Button, Flex, Input, useToast } from '@chakra-ui/react';
+import { useState } from 'react';
+
+const Home = () => {
+  const toast = useToast();
+  const [rowCount, setRowCount] = useState(0);
+  const [rows, setRows] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchRows = async () => {
+    if (rowCount > 2 && rowCount < 11) {
+      try {
+        setLoading(true);
+        const resposne = await fetch(`${process.env.REACT_APP_API_URL}/seats?count=${rowCount}`);
+        const data = await resposne.json();
+        setRows(data?.data?.seats);
+      } catch (error) {
+        toast({
+          duration: '2500',
+          title: error?.message || 'Something went wrong',
+          isClosable: true,
+          status: 'error',
+        });
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      toast({
+        duration: '2500',
+        title: 'Number of rows should be between 2 & 11',
+        isClosable: true,
+        status: 'error',
+      });
+    }
+  };
+
+  return (
+    <Flex as="main" align="center" justify="center" minH="99vh" bg="primary.bg" color="white">
+      {!rows ? (
+        <Box>
+          <Input
+            placeholder="Number of rows"
+            value={rowCount}
+            onChange={e => setRowCount(e.target.value)}
+          />
+          <Button colorScheme="teal" w="100%" mt="1rem" onClick={fetchRows} isLoading={loading}>
+            Submit
+          </Button>
+        </Box>
+      ) : (
+        <Flex> {/* <Box /> */}</Flex>
+      )}
+    </Flex>
+  );
+};
 
 export default Home;
